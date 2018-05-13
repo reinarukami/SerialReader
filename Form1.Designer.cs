ca5491bcab9c9a01262ae24e77238f2c2da3586b
Robin Sumalde <robin.sumalde@gmail.com>
2018-05-14 04:03:09 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SerialReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Led1CheckBox = new System.Windows.Forms.CheckBox();
+            this.Led2CheckBox = new System.Windows.Forms.CheckBox();
+            this.Led3CheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(317, 263);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Led1CheckBox
+            // 
+            this.Led1CheckBox.AutoSize = true;
+            this.Led1CheckBox.Location = new System.Drawing.Point(408, 55);
+            this.Led1CheckBox.Name = "Led1CheckBox";
+            this.Led1CheckBox.Size = new System.Drawing.Size(53, 17);
+            this.Led1CheckBox.TabIndex = 1;
+            this.Led1CheckBox.Text = "LED1";
+            this.Led1CheckBox.UseVisualStyleBackColor = true;
+            this.Led1CheckBox.CheckedChanged += new System.EventHandler(this.Led1CheckBox_CheckedChanged);
+            // 
+            // Led2CheckBox
+            // 
+            this.Led2CheckBox.AutoSize = true;
+            this.Led2CheckBox.Location = new System.Drawing.Point(408, 78);
+            this.Led2CheckBox.Name = "Led2CheckBox";
+            this.Led2CheckBox.Size = new System.Drawing.Size(53, 17);
+            this.Led2CheckBox.TabIndex = 2;
+            this.Led2CheckBox.Text = "LED2";
+            this.Led2CheckBox.UseVisualStyleBackColor = true;
+            this.Led2CheckBox.CheckedChanged += new System.EventHandler(this.Led2CheckBox_CheckedChanged);
+            // 
+            // Led3CheckBox
+            // 
+            this.Led3CheckBox.AutoSize = true;
+            this.Led3CheckBox.Location = new System.Drawing.Point(408, 101);
+            this.Led3CheckBox.Name = "Led3CheckBox";
+            this.Led3CheckBox.Size = new System.Drawing.Size(53, 17);
+            this.Led3CheckBox.TabIndex = 3;
+            this.Led3CheckBox.Text = "LED3";
+            this.Led3CheckBox.UseVisualStyleBackColor = true;
+            this.Led3CheckBox.CheckedChanged += new System.EventHandler(this.Led3CheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 364);
+            this.Controls.Add(this.Led3CheckBox);
+            this.Controls.Add(this.Led2CheckBox);
+            this.Controls.Add(this.Led1CheckBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Serial Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox Led1CheckBox;
+        private System.Windows.Forms.CheckBox Led2CheckBox;
+        private System.Windows.Forms.CheckBox Led3CheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using SerialReader.Class.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialReader
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+            DefaultSerialPort.serialPort.Open();
+            DefaultSerialPort.serialPort.DataReceived += DataRecievedHandler;
+        }
+
+        private void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            var test = DefaultSerialPort.serialPort.ReadLine(); 
+            richTextBox1.Invoke(new Action(() => richTextBox1.Text = richTextBox1.Text + test));
+        }
+
+        private void Led1CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(Led1CheckBox.Checked == true)
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED1ON\n");
+            }
+            else
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED1OF\n");
+            }
+        }
+
+        private void Led2CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Led2CheckBox.Checked == true)
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED2ON\n");
+            }
+            else
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED2OF\n");
+            }
+        }
+
+        private void Led3CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Led3CheckBox.Checked == true)
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED3ON\n");
+            }
+            else
+            {
+                DefaultSerialPort.serialPort.WriteLine("LED3OF\n");
+            }
+        }
+    }
+}
